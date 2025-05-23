553add20db5b8ccc36754d893cb3a7b0d89bbb91
Th3AI99la <talles.th54@gmail.com>
2025-05-23 12:51:19 -0300

    criado - Spring Boot

---
+++ src/main/java/com/drivecare/project/DrivecareApplication.java
@@ -0,0 +1,13 @@
+package com.drivecare.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivecareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivecareApplication.class, args);
+	}
+
+}
